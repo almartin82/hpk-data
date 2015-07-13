137bc95daa8e2d8c417cadfb7389750b5ea1e013
almartin82 <almartin@gmail.com>
2015-07-13 00:33:04 -0400

    add mandrill receipt

--- cur_stats.py
+++ cur_stats.py
@@ -1,15 +1,17 @@
 #local stuff
-import yahoo_api, functions, resources, pasteee
+import yahoo_api, functions, resources
 
 #standard
 from getpass import getuser
 from datetime import date, timedelta
+from time import gmtime, strftime
 import os
 
 #external (remember to update requirements.txt for heroku)
 import yaml
 import pandas
 import tinys3
+import mandrill
 
 #heroku or local
 if getuser() == 'amartin':
@@ -23,6 +25,7 @@ if getuser() == 'amartin':
     session_handle = creds['session_handle']
     aws_access_key = creds['aws_access_key']
     aws_secret_access_key = creds['aws_secret_access_key']
+    mandrill_key = creds['mandrill_key']
 else:
     #remote == os.environ
     key = os.environ['consumer_key']
@@ -32,6 +35,8 @@ else:
     session_handle = os.environ['session_handle']
     aws_access_key = os.environ['aws_access_key']
     aws_secret_access_key = os.environ['aws_secret_access_key']
+    mandrill_key = os.environ['mandrill_key']
+
 
 #initialize a yahoo session
 y = yahoo_api.YahooAPI(
@@ -43,8 +48,8 @@ y = yahoo_api.YahooAPI(
 )
 
 d = resources.yr_2015
-#dd = [d[0] + timedelta(days=x) for x in range((d[1]-d[0]).days + 1)]
-dd = [d[0] + timedelta(days=x) for x in range((d[1]-d[0]).days - 85)]
+dd = [d[0] + timedelta(days=x) for x in range((d[1]-d[0]).days + 1)]
+#dd = [d[0] + timedelta(days=x) for x in range((d[1]-d[0]).days - 90)]
 
 stat_df = pandas.DataFrame()
 
@@ -57,5 +62,21 @@ for day in dd:
         stat_df = stat_df.append(df)
 
 #up to s3
+stat_df.to_csv('hpk_2015.csv', index=False)
 conn = tinys3.Connection(aws_access_key, aws_secret_access_key, tls=True)
-conn.upload('hpk_2015.csv', stat_df.to_csv(), 'hpk')
+f = open('hpk_2015.csv','rb')
+conn.upload('hpk_2015.csv', f, 'hpk')
+
+#send receipt email to ALM
+mandrill_client = mandrill.Mandrill(mandrill_key)
+message = {
+    'from_email': 'datarobot@hpkdiaspora.com',
+    'from_name': 'hpk data robot',
+    'subject': 'grabbed 2015 standings successfully',
+    'text': 'heroku process ran at ' + strftime("%Y-%m-%d %H:%M:%S", gmtime()),
+    'to': [
+        {'email': 'almartin@gmail.com',
+         'name': 'Andrew Martin',
+         'type': 'to'}]
+}
+result = mandrill_client.messages.send(message=message)
